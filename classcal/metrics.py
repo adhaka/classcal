689630c1e21ccf13099d6ddfeab827f64a244b69
Harshit Agrawal <harshitbme@gmail.com>
2022-09-06 23:07:29 +0300

    layout for repo

---
+++ classcal/metrics.py
@@ -0,0 +1,19 @@
+"""
+Metrics for calculating classification and calibration performance.
+"""
+
+def classification_report():
+    pass
+
+def calibration_report():
+    pass
+
+def all_reports():
+    pass
+
+def calculated_classification_metrics():
+    pass
+
+def calculated_calibration_metrics():
+    pass
+
